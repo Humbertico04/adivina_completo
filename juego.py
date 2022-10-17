b949e49147997fe4327a75a9d8301b7158bd8702
Humbertico04 <humbertico2004@gmail.com>
2022-10-18 00:28:28 +0200

    Intento de puntuación

--- juego.py
+++ juego.py
@@ -9,8 +9,14 @@ from entrada import (
 )
 from entrada.menú import color_asignado, menu_completo, menu_inicio, texto_personalizado
 from entrada.numero import numero_a_adivinar
+import math
 
-def jugar_una_vez(numero, minimo, maximo, vidas):
+def puntuacion(maximo, vidas):
+    puntuacion = math.log10(maximo)* 100 + vidas * 100
+    return puntuacion
+
+
+def jugar_una_vez(numero, minimo, maximo, vidas, puntos):
     intento = pedir_entrada_numero_delimitado("Adivine el numero", minimo, maximo)
     # Se comprueba si el intento es correcto o no
     if vidas == 1:
@@ -20,34 +26,37 @@ def jugar_una_vez(numero, minimo, maximo, vidas):
         print("Demasiado pequeño")
         minimo = intento + 1
         vidas-=1
+        puntos -= 100
+        print ("Tu puntuación es de {} puntos!".format(puntos))
         print("Le queda(n) {} intentos".format(vidas))
         victoria = False
     elif intento > numero:
         print("Demasiado grande")
         maximo = intento - 1
         vidas-=1
+        puntos -= 100
+        print ("Tu puntuación es de {} puntos!".format(puntos))
         print("Le queda(n) {} intentos".format(vidas))
         victoria = False
     else:
         color_asignado('green')(texto_personalizado("rectangles", "Victoria"))
         victoria = True
         minimo = maximo = intento
-    return victoria, minimo, maximo, vidas
-
+    return victoria, minimo, maximo, vidas, puntos
 
 def pedir_entrada_del_numero_incognita(minimo, maximo):
     return pedir_entrada_numero_delimitado("Introduzca el número a adivinar",
                                         minimo, maximo)
 
 
-def jugar_una_partida(numero, minimo, maximo, vidas):
+def jugar_una_partida(numero, minimo, maximo, vidas, puntos):
     while True:
         # Se entra en un bucle infinito
         # que permite jugar varias veces
 
-        victoria, minimo, maximo, vidas = jugar_una_vez(numero, minimo, maximo, vidas)
+        victoria, minimo, maximo, vidas, puntos = jugar_una_vez(numero, minimo, maximo, vidas, puntos)
         if (victoria):
-            return
+            return puntos
 
 
 def niveles():
@@ -68,15 +77,16 @@ def niveles():
         minimo = 0
         maximo = 1000000000000
         vidas = 35
-    return minimo, maximo, vidas
-
+    puntos = puntuacion(maximo, vidas)
+    return minimo, maximo, vidas, puntos
 
 def jugar():
-    minimo, maximo, vidas = niveles()
+    minimo, maximo, vidas, puntos = niveles()
     while True:
         numero = numero_a_adivinar(minimo, maximo)
-        jugar_una_partida(numero, minimo, maximo, vidas)
-        if not pedir_entrada_si_o_no("¿Desea jugar una nueva partida?"):
+        jugar_una_partida(numero, minimo, maximo, vidas, puntos)
+
+        if not pedir_entrada_si_o_no("Su puntuación ha sido de {} puntos!\n¿Desea jugar una nueva partida?".format(puntos)):
             print("¡Hasta pronto!")
             return
 
